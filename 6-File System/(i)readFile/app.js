b2cbfa4e1282a4f32983b3eb3d9682bb95d87858
Sandeep LS <110336567+SandeepLS@users.noreply.github.com>
2023-10-26 01:24:18 +0530

    Update app.js

--- 6-File System/(i)readFile/app.js
+++ 6-File System/(i)readFile/app.js
@@ -13,9 +13,21 @@ Explain:-
     });
 */
 
-Ex:-app.js
+Ex:-In Console:-app.js
 var fs = require('fs')
-
 fs.readFile('module.js', 'utf8', function(err, data){
     console.log(data);
 })
+
+-------------------------OR---------------------------------------
+Ex:-In web server:-app.js
+var http = require('http')
+var fs = require('fs')
+
+http.createServer(function(req,res){
+    fs.readFile('./module.js','utf8', function(err,data){
+    res.writeHead(200, {'Content-Type':'text/html'})
+    res.write(data);
+    res.end();
+});
+}).listen(8080);

e42d6e6cc938e8e9c623734df64a61a5b229e92a
Sandeep LS <110336567+SandeepLS@users.noreply.github.com>
2023-10-26 01:50:42 +0530

    Update app.js

--- 6-File System/(ii)writeFile/app.js
+++ 6-File System/(ii)writeFile/app.js
@@ -1,4 +1,10 @@
 /*
+Create Files:-
+            The File System module has methods for creating new files:
+            fs.appendFile()
+            fs.open()
+            fs.writeFile()
+
 writeFile have 3 parameter first(create a new file for writing('module.js'),
                            second(write a content which u want, in a new file(console.log("Hi Sandeep");) ),
                            third(after complet this above job,/ if u want to print something 
@@ -8,7 +14,25 @@ writeFile have 3 parameter first(create a new file for writing('module.js'),
 */
 Ex:-
 var fs = require('fs')
-
 fs.writeFile('module.js','console.log("Hi Sandeep")', function(err){
     console.log("Data Saved");
 })
+----------------------------------OR---------------------------------------
+var fs = require('fs');
+fs.writeFile('mynewfile3.txt', 'Hello content!', function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
+----------------------------------OR---------------------------------------
+var fs = require('fs');
+fs.appendFile('mynewfile0.txt', 'Hello content!', function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
+----------------------------------OR---------------------------------------
+var fs = require('fs');
+fs.open('mynewfile5.txt', 'w', function (err, file) {
+  if (err) throw err;
+  console.log('Saved!');
+});
+  

0e1ea080bdfe801951883e8b24313a1893f179ac
Sandeep LS <110336567+SandeepLS@users.noreply.github.com>
2023-10-26 02:09:39 +0530

    Update app.js

--- 6-File System/unlink(DeletedFile)/app.js
+++ 6-File System/unlink(DeletedFile)/app.js
@@ -4,3 +4,9 @@ fs.unlink('mmmm.js', function(err){
     console.log("Deleted");
 })
 //after execution mmmm.js file have deleted.
+-------------------------------OR------------------------------
+var fs = require('fs');
+fs.unlink('mynewfile2.txt', function (err) {
+  if (err) throw err;
+  console.log('File deleted!');
+});

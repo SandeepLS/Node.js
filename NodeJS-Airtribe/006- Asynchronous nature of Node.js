73952dde418f8fc7e3328a1c2b45b1aacaf5e4c8
Sandeep LS <110336567+SandeepLS@users.noreply.github.com>
2024-09-25 11:22:04 +0530

    Create 006- Asynchronous nature of Node.js

---
+++ NodeJS-Airtribe/006- Asynchronous nature of Node.js
@@ -0,0 +1,8 @@
+Asynchronous/Non-blocking  nature of Node.js:-
+NodeJS is single threaded, meaning it doesn't support mylti threaded.
+However NodeJS application are highly scalabel, due to the Asynchronous nature of NodeJS.(Doing other task, while one task to complete)
+  Ex:- washing our cloths in washing machine and start in, & in a synchronous system we would sit and watch the machine, until it finishes(we can't do anything else).
+       that's not very effecient our time, insted by washing machine is running, we gohead do other task may be we read a book, have a cup of coffe, 
+       we are not stuck waiting for the washing machine to finish it. we are free to do other task. & when the washing is done we are take out the cloths, and hung them to try.
+  
+       
